fa9ab8e8e64f5d8da1e31c65aa05ca8ed76bd9ca
nantenainarakotozafy3-hub <nantenainarakotozafy3@gmail.com>
2026-03-11 14:21:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const cacheName = 'hira-fanantenana-v2';
+const cacheName = 'sampana-v2';
 const assets = [
   './',
   './index.html',
   './style.css',
   './script.js',
+  './manifest.json',
   './lyrics.json',
-  './logo.png',
-  './back-icon.png',
-  './manifest.json'
+  './play.png',
+  './pause.png',
+  './stop.png',
+  './logo.png'
 ];
 
 self.addEventListener('install', e => {
@@ -16,13 +18,19 @@ self.addEventListener('install', e => {
       return cache.addAll(assets);
     })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  if (url.pathname.endsWith('.mp3')) {
+    return;
+  }
+
   e.respondWith(
     caches.match(e.request).then(response => {
       return response || fetch(e.request);
     })
   );
-
 });
