3a12ada2e8b7249e6ea7e384af51a178d346c474
nantenainarakotozafy3-hub <nantenainarakotozafy3@gmail.com>
2026-03-13 15:49:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'hira-fanantenana-v10';
+const cacheName = 'hira-fanantenana-v20';
 const assets = [
   './',
   './index.html',
@@ -7,14 +7,12 @@ const assets = [
   './lyrics.json',
   './logo.png',
   './back-icon.png',
-  './play.png',
-  './pause.png',
-  './stop.png',
   './manifest.json'
-];self.addEventListener('install', e => {
+];
+
+self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('Caching assets...');
       return cache.addAll(assets);
     })
   );
@@ -35,20 +33,7 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
-
-      if (response) return response;
-
-      return fetch(e.request).then(fetchRes => {
-        return caches.open(cacheName).then(cache => {
-
-          if (e.request.url.includes('.mp3')) {
-             cache.put(e.request.url, fetchRes.clone());
-          }
-          return fetchRes;
-        });
-      });
-    }).catch(() => 
-        console.log("Offline ary tsy hita ny hira");
+      return response || fetch(e.request);
     })
   );
 });
