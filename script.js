195d1dc350172698cd54d840d4df813f5e6d2016
nantenainarakotozafy3-hub <nantenainarakotozafy3@gmail.com>
2026-03-18 10:43:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ function openSong(song) {
     document.getElementById("songPage").style.display = "block";
     document.getElementById("songTitle").textContent = song.title;
     document.getElementById("lyrics").innerText = song.lyrics.join("\n");
+    document.getElementById("currentTime").textContent = "0:00";
+    document.getElementById("durationTime").textContent = "0:00";
+}
 
     if (song.audio) {
         player.src = "AUDIO/" + song.audio; 
@@ -42,17 +45,21 @@ function openSong(song) {
     window.scrollTo(0, 0);
 }
 
+function formatTime(seconds) {
+    let min = Math.floor(seconds / 60);
+    let sec = Math.floor(seconds % 60);
+    if (sec < 10) sec = "0" + sec;
+    return min + ":" + sec;
+}
+
 player.addEventListener("timeupdate", () => {
     if (player.duration) {
         const progress = (player.currentTime / player.duration) * 100;
         progressBar.value = progress;
-    }
-});
 
-progressBar.addEventListener("input", () => {
-    if (player.duration) {
-        const seekTime = (progressBar.value / 100) * player.duration;
-        player.currentTime = seekTime;
+        document.getElementById("currentTime").textContent = formatTime(player.currentTime);
+
+        document.getElementById("durationTime").textContent = formatTime(player.duration);
     }
 });
 
